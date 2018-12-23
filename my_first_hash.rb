b39d32e363d6b0f1e076bb3979d124d4d1f5695a
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-23 22:49:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,6 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  
+  your_hash = {"Name" => "Tominiyi Ajayi", "Age" => "32", "Nationalty" => "British"}
 
   
 end
@@ -9,7 +10,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
 
 end
 
@@ -31,7 +32,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets"] 
 
 
   # add 4 gun powder to the shipping_manifest hash below
